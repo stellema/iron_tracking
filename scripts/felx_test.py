2046aa9513f62bb2e65b958d4d5732b78152a774
Annette Stellema <40450353+stellema@users.noreply.github.com>
2022-12-24 12:17:52 +1100

    Update fncs.py & felx_test.py for parcels2.2.1

--- scripts/felx_test.py
+++ scripts/felx_test.py
@@ -34,53 +34,130 @@ from fncs import (get_datetime_bounds, get_ofam_filenames, from_ofam3,
                   add_fset_constants, add_Kd490_field)
 
 
-exp = 0
 
-# Simulation runtime paramaters.
-dt = timedelta(hours=48)  # Advection step (negative for backward).
-outputdt = timedelta(days=2)  # Advection steps to write.
-runtime = timedelta(days=20)
+def test_fieldset_initialisation():
+    cs = 300
+    exp = 0
+    variables = cfg.bgc_name_map
+    variables = {'P': 'phy', 'Z': 'zoo', 'Det': 'det',
+                 'temp': 'temp', 'Fe': 'fe', 'N': 'no3'}
+    fieldset = ofam3_fieldset(exp=exp, cs=cs)
+    fieldset = add_ofam3_bgc_fields(fieldset, variables, exp)
+    fieldset = add_Kd490_field(fieldset)
+    fieldset = add_fset_constants(fieldset)
+    fieldset.computeTimeChunk(0, timedelta(hours=48).seconds)
+    return fieldset
 
-# Create Fieldset.
-cs = 300
-variables = cfg.bgc_name_map
-variables = {'P': 'phy', 'Z': 'zoo', 'Det': 'det', 'temp': 'temp', 'Fe': 'fe', 'N': 'no3'}
-fieldset = ofam3_fieldset(exp=exp, cs=cs)
-fieldset = add_ofam3_bgc_fields(fieldset, variables, exp)
-fieldset = add_Kd490_field(fieldset)
-fieldset = add_fset_constants(fieldset)
-fieldset.computeTimeChunk(0, dt.seconds)
 
-# Create ParticleClass.
-pclass = get_fe_pclass(fieldset)
+def test_pset_creation():
+    exp = 0
+    # Simulation runtime paramaters.
+    dt = timedelta(hours=48)  # Advection step (negative for backward).
+    outputdt = timedelta(days=2)  # Advection steps to write.
+    runtime = timedelta(days=20)
 
-# Create ParticleSet.
-pset_start = fieldset.P.grid.time[0]
-npart = 3
-times = np.repeat(pset_start, npart)
-# lats = np.repeat(-5.1, npart)
-# depths = np.repeat(150, npart)
-# lons = np.linspace(153, 154, npart)
-lats = np.repeat(0, npart)
-depths = np.repeat(150, npart)
-lons = np.linspace(165, 170, npart)
-pset = ParticleSet.from_list(fieldset=fieldset, pclass=pclass,
-                             lon=lons, lat=lats, depth=depths, time=times,
-                             lonlatdepth_dtype=np.float32)
+    # Create Fieldset.
+    cs = 300
+    variables = cfg.bgc_name_map
+    variables = {'P': 'phy', 'Z': 'zoo', 'Det': 'det', 'temp': 'temp', 'Fe': 'fe', 'N': 'no3'}
+    fieldset = ofam3_fieldset(exp=exp, cs=cs)
+    fieldset = add_ofam3_bgc_fields(fieldset, variables, exp)
+    fieldset = add_Kd490_field(fieldset)
+    fieldset = add_fset_constants(fieldset)
+    fieldset.computeTimeChunk(0, dt.seconds)
 
-# Kernels.
-kernels = pset.Kernel(AdvectionRK4_3D)
-kernels += (pset.Kernel(IronSourceInput) + pset.Kernel(IronScavenging) +
-            pset.Kernel(IronRemineralisation) + pset.Kernel(IronPhytoUptake)
-            + pset.Kernel(Iron))
+    # Create ParticleClass.
+    pclass = get_fe_pclass(fieldset)
+    return fieldset, pclass
 
-# # Create output ParticleFile p_name and time steps to write output.
-# output_file = pset.ParticleFile(cfg.data/'test.nc', outputdt=outputdt)  # @todo
 
-print(pset)
-start = fieldset.time_origin.time_origin  # @todo
-# ParticleSet execution endtime.
-endtime = int(pset_start + runtime.total_seconds())  # @todo
-pset.execute(kernels, endtime=endtime, dt=dt, #output_file=output_file,
-              verbose_progress=True, recovery=recovery_kernels)
-print(pset)
+def test_forward_particle_execution():
+    exp = 0
+    # Simulation runtime paramaters.
+    dt = timedelta(hours=48)  # Advection step (negative for backward).
+    outputdt = timedelta(days=2)  # Advection steps to write.
+    runtime = timedelta(days=20)
+
+    # Create Fieldset.
+    cs = 300
+    variables = cfg.bgc_name_map
+    fieldset = ofam3_fieldset(exp=exp, cs=cs)
+    fieldset = add_ofam3_bgc_fields(fieldset, variables, exp)
+    fieldset = add_Kd490_field(fieldset)
+    fieldset = add_fset_constants(fieldset)
+    fieldset.computeTimeChunk(0, dt.seconds)
+
+    # Create ParticleClass.
+    pclass = get_fe_pclass(fieldset)
+    # Create ParticleSet.
+    pset_start = fieldset.P.grid.time[0]
+    npart = 3
+    times = np.repeat(pset_start, npart)
+    lats = np.repeat(0, npart)
+    depths = np.repeat(150, npart)
+    lons = np.linspace(165, 170, npart)
+    pset = ParticleSet.from_list(fieldset=fieldset, pclass=pclass,
+                                 lon=lons, lat=lats, depth=depths, time=times,
+                                 lonlatdepth_dtype=np.float32)
+
+    # Kernels.
+    kernels = pset.Kernel(AdvectionRK4_3D)
+    kernels += (pset.Kernel(IronSourceInput) + pset.Kernel(IronScavenging) +
+                pset.Kernel(IronRemineralisation) + pset.Kernel(IronPhytoUptake)
+                + pset.Kernel(Iron))
+
+    # # Create output ParticleFile p_name and time steps to write output.
+    # output_file = pset.ParticleFile(cfg.data/'test.nc', outputdt=outputdt)  # @todo
+
+    print(pset)
+    start = fieldset.time_origin.time_origin  # @todo
+    # ParticleSet execution endtime.
+    endtime = int(pset_start + runtime.total_seconds())  # @todo
+    pset.execute(kernels, endtime=endtime, dt=dt, #output_file=output_file,
+                  verbose_progress=True, recovery=recovery_kernels)
+    print(pset)
+
+
+
+def test_pset_from_file():
+    exp = 0
+    cs = 300
+    variables = cfg.bgc_name_map
+    fieldset = ofam3_fieldset(exp=exp, cs=cs)
+    fieldset = add_ofam3_bgc_fields(fieldset, variables, exp)
+    fieldset = add_Kd490_field(fieldset)
+    fieldset = add_fset_constants(fieldset)
+
+    # Create ParticleClass.
+    pclass = get_fe_pclass(fieldset)
+
+    # Pset from file
+    filename = cfg.data / 'plx/plx_hist_165_v1r00.nc'
+    df = xr.open_dataset(str(filename), decode_cf=False)
+    df = df.isel(traj=slice(10)).dropna('obs', 'all')  # !!!
+
+    df_vars = [v for v in df.data_vars]
+
+    vars = {}
+
+    for v in ['trajectory', 'time', 'lat', 'lat', 'z']:
+        vars[v] = np.ma.filled(df.variables[v], np.nan)
+
+    vars['depth'] = np.ma.filled(df.variables['z'], np.nan)
+    vars['id'] = np.ma.filled(df.variables['trajectory'], np.nan)
+
+    if isinstance(vars['time'][0], np.timedelta64):
+        vars['time'] = np.array([t / np.timedelta64(1, 's') for t in vars['time']])
+
+    for v in pclass.getPType().variables:
+        if v.name in df_vars:
+            vars[v.name] = np.ma.filled(df.variables[v.name], np.nan)
+        elif v.name not in ['xi', 'yi', 'zi', 'ti', 'dt', '_next_dt',
+                            'depth', 'id', 'fileid', 'state'] \
+                and v.to_write:
+            raise RuntimeError('Variable %s is in pclass but not in the particlefile' % v.name)
+        # to_write[v.name] = v.to_write
+
+# fieldset = test_fieldset_initialisation()
+# fieldset, pclass = test_pset_creation
+# test_forward_particle_execution()
--- scripts/fncs.py
+++ scripts/fncs.py
@@ -24,6 +24,7 @@ import math
 import parcels
 import numpy as np
 import xarray as xr
+from calendar import monthrange
 from datetime import datetime, timedelta
 from parcels import (FieldSet, ParticleSet, VectorField, Variable, ScipyParticle,
                      JITParticle, fieldfilebuffer)
@@ -52,8 +53,8 @@ def get_ofam_filenames(var, times):
 
 
 def from_ofam3(filenames, variables, dimensions, indices=None, mesh='spherical',
-               allow_time_extrapolation=None, time_periodic=False,
-               tracer_interp_method='bgrid_tracer', chunksize=None, **kwargs):
+               allow_time_extrapolation=None, field_chunksize='auto', time_periodic=False,
+               tracer_interp_method='bgrid_tracer', **kwargs):
     """Initialise FieldSet object from NetCDF files of OFAM3 fields.
 
     Args:
@@ -95,10 +96,9 @@ def from_ofam3(filenames, variables, dimensions, indices=None, mesh='spherical',
                                     mesh=mesh, indices=indices,
                                     time_periodic=time_periodic,
                                     allow_time_extrapolation=allow_time_extrapolation,
+                                    field_chunksize=field_chunksize,
                                     interp_method=interp_method,
-                                    gridindexingtype='mom5',
-                                    chunkdims_name_map=cfg.chunkdims_name_map,
-                                    chunksize=chunksize, **kwargs)
+                                    gridindexingtype='mom5', **kwargs)
 
     if hasattr(fieldset, 'W'):
         fieldset.W.set_scaling_factor(-1)
@@ -126,8 +126,8 @@ def ofam3_fieldset(exp=0, cs=300):
 
     """
     # Add OFAM3 dims to chunk name map.
-    fieldfilebuffer.DaskFileBuffer.add_to_dimension_name_map_global(
-        cfg.chunkdims_name_map)
+    # fieldfilebuffer.DaskFileBuffer.add_to_dimension_name_map_global(
+    #     cfg.chunkdims_name_map)
 
     # Dictionary mapping variables to variable names in the netCDF file(s).
     variables = {'U': 'u', 'V': 'v', 'W': 'w'}
@@ -147,16 +147,18 @@ def ofam3_fieldset(exp=0, cs=300):
 
     # Size of the chunks in dask loading.
     if cs in ['auto', False]:
-        chunksize = cs
+        chunks = cs
+        nmap = None
     else:
-        chunksize = {}
-        for v in variables:
-            chunksize[v] = {'time': ('Time', 1), 'depth': ('sw_ocean', 1),
-                            'lat': ('yu_ocean', cs), 'lon': ('xu_ocean', cs)}
+        nmap = cfg.chunkdims_name_map
+        # field_chunksize.
+        chunks = {'Time': 1,
+                  'sw_ocean': 1, 'st_ocean': 1, 'depth': 1,
+                  'yt_ocean': cs, 'yu_ocean': cs, 'lat': cs,
+                  'xt_ocean': cs, 'xu_ocean': cs, 'lon': cs}
 
-    kwargs = dict(deferred_load=True)
-    fieldset = from_ofam3(filenames, variables, dimensions, chunksize=chunksize,
-                          **kwargs)
+    kwargs = dict(field_chunksize=chunks, chunkdims_name_map=nmap, deferred_load=True)
+    fieldset = from_ofam3(filenames, variables, dimensions, **kwargs)
     return fieldset
 
 
@@ -187,9 +189,9 @@ def add_ofam3_bgc_fields(fieldset, variables, exp):
         filenames[v] = {'depth': mesh, 'lat': mesh, 'lon': mesh, 'data': f}
 
     # Size of the chunks in dask loading.
-    chunksize = fieldset.U.grid.chunksize
+    chunks = fieldset.get_fields()[0].field_chunksize
 
-    xfieldset = from_ofam3(filenames, variables, dimensions, chunksize=chunksize)
+    xfieldset = from_ofam3(filenames, variables, dimensions, field_chunksize=chunks)
 
     # Field time origins and calander.
     xfieldset.time_origin = fieldset.time_origin
@@ -207,11 +209,44 @@ def format_Kd490_dataset():
     """Merge & add time dimension to Kd490 monthly files."""
     # Open kd490 dataset.
     files = [cfg.obs / 'GMIS_Kd490/GMIS_S_K490_{:02d}.nc'.format(m) for m in range(1, 13)]
-    ds = xr.open_mfdataset(files, combine='nested', concat_dim='time')
-    ds['time'] = np.array([datetime(1981, i+1, 1, 12) for i in range(12)],
-                          dtype='datetime64[ns]')
+    df = xr.open_mfdataset(files, combine='nested', concat_dim='time', decode_cf=False)
+
+    # Create New Dataset.
+    # Time bounds.
+    time = np.array([np.datetime64('1981-{:02d}-16'.format(i+1)) for i in range(12)],
+                    dtype='datetime64[ns]')
+    time_bnds = np.array([['1981-{:02d}-01T12'.format(m),
+                           '1981-{:02d}-{:02d}T12'.format(m, monthrange(1981, m)[-1])]
+                          for m in range(1, 13)], dtype='datetime64[ns]')
+
+    time_attrs = {'bounds': 'time_bnds',
+                  # 'units': 'seconds since 1981-01-01 00:00:00',
+                  # 'calendar': '365_day',
+                  'axis': 'T',
+                  'long_name': 'time',
+                  'standard_name': 'time'
+                  }
+    coords = {'lat': df.lat, 'lon': df.lon, 'time': (['time'], time, time_attrs),
+              'time_bnds': (['time', 'nv'], time_bnds), 'nv': [1., 2.]}
+
+    attrs = {'Conventions': 'CF-1.7',
+             'title': df.attrs['Title'],
+             'history': df.attrs['history'],
+             'institution': 'Joint Research Centre (IES)',
+             'source': 'https://data.europa.eu/data/datasets/dfe3524f-7d57-486c-ac92-cfa730887c48?locale=en',
+             'comment': 'Monthly climatology sea surface diffuse attenuation coefficient at 490nm in m^-1 (log10 scalling) derived from the SeaWiFS sensor.',
+             'references': 'Werdell, P.J. (2005). Ocean color K490 algorithm evaluation. http://oceancolor.gsfc.nasa.gov/REPROCESSING/SeaWiFS/R5.1/k490_update.html'
+             }
+
+    ds = xr.Dataset({'Kd490': (['time', 'lat', 'lon'], df['Kd490'].values, df['Kd490'].attrs)},
+                    coords=coords, attrs=attrs)
+
+    ds.time.encoding = {'units': 'seconds since 1981-01-01 00:00:00'}
+
     # Save dataset.
-    ds.to_netcdf(cfg.obs / 'GMIS_Kd490/GMIS_S_Kd490.nc')
+    ds.to_netcdf(cfg.obs / 'GMIS_Kd490/GMIS_S_Kd490.nc', format='NETCDF4',
+                 encoding={'Kd490': {'shuffle': True, 'chunksizes': [1, 3503, 9577],
+                                     'zlib': True, 'complevel': 5}})
 
     # # Interpolate lat and lon to OFAM3
     # # Open OFAM3 mesh coordinates.
@@ -222,7 +257,7 @@ def format_Kd490_dataset():
     # plt.tight_layout()
     # plt.savefig(cfg.fig / 'particle_source_map.png', bbox_inches='tight',
     #             dpi=300)
-    files = cfg.obs / 'SEAWIFS_Kd490/SEASTAR_SEAWIFS_GAC.19980801_20100831.L3b.MC.KD.nc'
+    files = cfg.obs / 'GMIS_Kd490/GMIS_S_Kd490.nc'
     ds = xr.open_dataset(files)
 
 
@@ -242,11 +277,11 @@ def add_Kd490_field(fieldset):
     dimensions = {'lon': 'lon', 'lat': 'lat', 'time': 'time'}
 
     # Size of the chunks in dask loading.
-    chunksize = fieldset.get_fields()[0].grid.chunksize
+    chunks = fieldset.get_fields()[0].field_chunksize
+    time_periodic = timedelta(days=365).total_seconds()
 
-    xfieldset = FieldSet.from_netcdf(filenames, variables, dimensions, chunksize=chunksize,
-                                     time_periodic=timedelta(days=365))
-
+    xfieldset = FieldSet.from_netcdf(filenames, variables, dimensions,
+                                     field_chunksize=chunks, time_periodic=time_periodic)
     # Field time origins and calander.
     xfieldset.time_origin = fieldset.time_origin
     xfieldset.time_origin.time_origin = fieldset.time_origin.time_origin
