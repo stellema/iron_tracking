5156a9e840817ed253465e31159f92c8d8c79b1e
Annette Stellema <40450353+stellema@users.noreply.github.com>
2023-01-23 10:20:43 +1100

    Update datasets.py
    Add plx functions

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -25,7 +25,7 @@ from datetime import datetime, timedelta
 import cfg
 
 
-def get_plx_filename(exp, lon, r, v=1):
+def get_plx_filename(exp, lon, r, v=1, folder='plx/'):
     """Get plx particle filename.
 
     Args:
@@ -38,12 +38,20 @@ def get_plx_filename(exp, lon, r, v=1):
         filename (pathlib.Path): plx dataset filename (path to plx repo).
 
     """
-    filename = 'data/plx/plx_{}_{}_v1r{:02d}.nc'.format(cfg.exp_abr[exp], lon, r)
-    filename = cfg.plx / filename
+    if r is not None:
+        file = 'plx_{}_{}_v{}r{:02d}.nc'.format(cfg.exp_abr[exp], lon, v, r)
+    else:
+        file = 'plx_{}_{}_v{}.nc'.format(cfg.exp_abr[exp], lon, v)
+
+    path = cfg.plx / 'data'
+    if folder is not None:
+        path = path / folder
+
+    filename = path / file
     return filename
 
 
-def get_felx_filename(exp, lon, r, v=1, subfolder=None):
+def get_felx_filename(exp, lon, r, v=1, folder=None):
     """Get felx particle filename and path.
 
     e.g., repo/felx/data/subfolder/felx_hist_165_vXrYY.nc
@@ -60,8 +68,8 @@ def get_felx_filename(exp, lon, r, v=1, subfolder=None):
 
     """
     filename = 'felx_{}_{}_v{}r{:02d}.nc'.format(cfg.exp_abr[exp], lon, v, r)
-    if subfolder is not None:
-        filename = subfolder + filename
+    if folder is not None:
+        filename = folder + filename
 
     filename = cfg.data / filename
     return filename
@@ -131,6 +139,10 @@ def ofam3_datasets(exp, variables=cfg.bgc_name_map):
         - Add Kd490
 
     """
+    # Dictionary mapping variables to file(s).
+    if isinstance(variables, dict):
+        variables = list(variables.values)
+
     dims_name_map = {'lon': ['xt_ocean', 'xu_ocean'],
                      'lat': ['yt_ocean', 'yu_ocean'],
                      'depth': ['st_ocean', 'sw_ocean']}
@@ -152,11 +164,11 @@ def ofam3_datasets(exp, variables=cfg.bgc_name_map):
               'yt_ocean': cs, 'yu_ocean': cs, 'lat': cs,
               'xt_ocean': cs, 'xu_ocean': cs, 'lon': cs}
 
-    nmonths = 2 if any([i for i in variables.keys() if i in cfg.bgc_name_map.keys()]) else 12
-    # Dictionary mapping variables to file(s).
+    nmonths = 2 if any([v for v in variables if v in ['fe', 'no3', 'temp']]) else 12
+
     files = []
-    for v in variables:
-        f = get_ofam_filenames(variables[v], get_datetime_bounds(exp, nmonths))
+    for var in variables:
+        f = get_ofam_filenames(var, get_datetime_bounds(exp, nmonths))
         files.append(f)
     files = np.concatenate(files)
 
@@ -169,29 +181,143 @@ def ofam3_datasets(exp, variables=cfg.bgc_name_map):
     return ds
 
 
+def concat_exp_dimension(ds, add_diff=False):
+    """Concatenate list of datasets along 'exp' dimension.
+
+    Args:
+        ds (list of xarray.Dataset): List of datasets.
+
+    Returns:
+        ds (xarray.Dataset): Concatenated dataset.
+
+    """
+    if add_diff:
+        for var in ['time', 'rtime']:
+            if var in ds[0].dims:
+                if ds[0][var].size == ds[-1][var].size:
+                    for i in range(len(ds)):
+                        ds[i][var] = ds[0][var]
+
+    if add_diff:
+        # Calculate projected change (RCP-hist).
+        ds = [*ds, ds[1] - ds[0]]
+
+    ds = [ds[i].expand_dims(dict(exp=[i])) for i in range(len(ds))]
+    ds = xr.concat(ds, 'exp')
+    return ds
+
+
+def combine_source_indexes(ds, z1, z2):
+    """Merge the values of two sources in source dataset.
+
+    Args:
+        ds (xarray.Dataset): Particle source dataset.
+        z1 (int): Source ID coordinate to merge (keep this index).
+        z2 (int): Source ID coordinate to merge.
+
+    Returns:
+        ds_new (TYPE): Reduced Dataset.
+
+    Notes:
+        - Adds the values in 'zone' position z1 and z2.
+        - IDs do not have to match positional indexes.
+
+    """
+    # Indexes of unchanged zones (concat to updated DataArray).
+    z_keep = ds.zone.where((ds.zone != z1) & (ds.zone != z2), drop=True)
+
+    # New dataset with zone z2 dropped (added zone goes to z1).
+    ds_new = ds.sel(zone=ds.zone.where(ds.zone != z2, drop=True)).copy()
+
+    # Add values in z2 and z2, then concat to ds_new.
+    for var in ds.data_vars:
+        dx = ds[var]
+        if 'zone' in dx.dims:
+
+            # Merge ragged arrays.
+            if 'traj' in dx.dims:
+                dx = dx.sel(zone=z1).combine_first(dx.sel(zone=z2, drop=1))
+
+            # Sum arrays.
+            elif 'rtime' in dx.dims:
+                dx = dx.sel(zone=z1) + dx.sel(zone=z2, drop=1)
+
+            # Sum elements.
+            elif dx.ndim == 1:
+                tmp = dx.sel(zone=z1).item() + dx.sel(zone=z2).item()
+                dx = xr.DataArray(tmp, coords=dx.sel(zone=z1).coords)
+
+            # Concat merged zone[z1] to zone[z_keep].
+            ds_new[var] = xr.concat([dx, ds[var].sel(zone=z_keep)], dim='zone')
+
+    return ds_new
+
+
+def merge_interior_sources(ds):
+    """Merge longitudes of source North/South Interior.
+
+    Merge longitudes:
+        - North interior: zone[6] = zone[6+7+8+9+10].
+        - South interior lons: zone[8] = zone[12+13+14+15].
+    Notes:
+        - Modified to skip South interior (<165E).
+
+    """
+    nlons = [5, 5]  # Number of interior lons to merge [South, North].
+    zi = [7, 12]   # Zone indexes to merge into [South, North].
+    zf = [7, 8]  # New zone positions [South, North].
+
+    # Iteratively combine next "source" index into first (z1).
+    for i in range(2):  # [South, North].
+        for a in range(1, nlons[i]):
+            ds = combine_source_indexes(ds, zi[i], zi[i] + a)
+
+    # Reset source name and colours.
+    if set(['names', 'colors']).issubset(ds.data_vars):
+        for i in zf:
+            ds['names'][i] = cfg.zones.names[i]
+            ds['colors'][i] = cfg.zones.colors[i]
+
+    ds.coords['zone'] = np.arange(ds.zone.size, dtype=int)
+    return ds
+
+
 def format_Kd490_dataset():
     """Merge & add time dimension to Kd490 monthly files."""
+    time_dim = ['time', 'month'][1]
     # Open kd490 dataset.
     files = [cfg.obs / 'GMIS_Kd490/GMIS_S_K490_{:02d}.nc'.format(m) for m in range(1, 13)]
-    df = xr.open_mfdataset(files, combine='nested', concat_dim='time', decode_cf=False)
+    df = xr.open_mfdataset(files, combine='nested', concat_dim='time', decode_cf=0)
 
     # Create New Dataset.
-    # Time bounds.
-    time = np.array([np.datetime64('1981-{:02d}-16'.format(i+1)) for i in range(12)],
-                    dtype='datetime64[ns]')
-    time_bnds = np.array([['1981-{:02d}-01T12'.format(m),
-                           '1981-{:02d}-{:02d}T12'.format(m, monthrange(1981, m)[-1])]
-                          for m in range(1, 13)], dtype='datetime64[ns]')
+    # Format longitudes.
+    df = df.assign_coords(lon=df.lon % 360)
+    df = df.sortby(df.lon)
 
-    time_attrs = {'bounds': 'time_bnds',
-                  # 'units': 'seconds since 1981-01-01 00:00:00',
-                  # 'calendar': '365_day',
-                  'axis': 'T',
-                  'long_name': 'time',
-                  'standard_name': 'time'
-                  }
-    coords = {'lat': df.lat, 'lon': df.lon, 'time': (['time'], time, time_attrs),
-              'time_bnds': (['time', 'nv'], time_bnds), 'nv': [1., 2.]}
+    # Format time coord.
+    # Keep as months.
+    if time_dim == 'month':
+        time = np.arange(1, 13, dtype=np.float32)
+        time_bnds = np.array([[a, b] for a, b in zip(np.roll(time, 1), np.roll(time, -1))])
+        time_attrs = {'axis': 'T', 'long_name': 'time', 'standard_name': 'time'}
+    else:
+        # Alt: Convert time to dates.
+        y = 1981
+        time = [np.datetime64('{}-{:02d}-16'.format(y, m)) for m in range(1, 13)]
+        time = np.array(time, dtype='datetime64[ns]')
+
+        time_bnds = [['{}-{:02d}-01T12'.format(y, m),
+                      '{}-{:02d}-{:02d}T12'.format(y, m, monthrange(1981, m)[-1])]
+                     for m in range(1, 13)]
+        time_bnds = np.array(time_bnds, dtype='datetime64[ns]')
+
+        time_attrs = {'bounds': 'time_bnds', 'axis': 'T',
+                      # 'units': 'seconds since 1981-01-01 00:00:00',
+                      # 'calendar': '365_day',
+                      'long_name': 'time', 'standard_name': 'time'}
+
+    coords = {'lat': df.lat, 'lon': df.lon, time_dim: ([time_dim], time, time_attrs),
+              'time_bnds': ([time_dim, 'nv'], time_bnds), 'nv': [1., 2.]}
 
     attrs = {'Conventions': 'CF-1.7',
              'title': df.attrs['Title'],
@@ -208,7 +334,7 @@ def format_Kd490_dataset():
     ds.time.encoding = {'units': 'seconds since 1981-01-01 00:00:00'}
 
     # Save dataset.
-    ds.to_netcdf(cfg.obs / 'GMIS_Kd490/GMIS_S_Kd490.nc', format='NETCDF4',
+    ds.to_netcdf(cfg.obs / 'GMIS_Kd490/GMIS_S_Kd490_{}.nc'.format(time_dim), format='NETCDF4',
                  encoding={'Kd490': {'shuffle': True, 'chunksizes': [1, 3503, 9577],
                                      'zlib': True, 'complevel': 5}})
 
