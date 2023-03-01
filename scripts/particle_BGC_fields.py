59be6c7353db357321f4701f23ffa099701b321a
Annette Stellema <40450353+stellema@users.noreply.github.com>
2023-03-01 16:16:54 +1100

    Update particle_BGC_fields.py

--- scripts/particle_BGC_fields.py
+++ scripts/particle_BGC_fields.py
@@ -210,7 +210,7 @@ def parallelise_BGC_fields(exp):
     if rank == 0:
         # Input ([[var0, 0], [var0, 1], ..., [varn, n]).
         # TODO: put pack to range(pds.num_subsets) & calc last subset
-        data = [[v, i] for v in pds.bgc_variables for i in range(pds.num_subsets - 2)]  # len=35
+        data = [[v, i] for v in pds.bgc_variables for i in range(pds.num_subsets - 1)]  # len=35
     else:
         data = None
     data = comm.scatter(data, root=0)
